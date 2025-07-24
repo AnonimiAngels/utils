2d2b41423493bffb40935e0687e6a93cdd49ca2e
AnonimiAngels <gutsulyak.vladyslav2000@gmail.com>
2025-07-24 12:25:49 +0200

    Update shared_mutex.hpp
    added missing license, 
    added NDEBUG for destructor checks

--- inc/common/shared_mutex.hpp
+++ inc/common/shared_mutex.hpp
@@ -2,6 +2,34 @@
 #ifndef SHARED_MUTEX_HPP
 #define SHARED_MUTEX_HPP
 
+/**
+ * @file shared_mutex.hpp
+ * @author anonimi_angels (gutsulyak.vladyslav2000@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2025-7-24
+ *
+ * @copyright
+ *The MIT License (MIT)
+ *Copyright (c) 2025 anonimi_angels
+ *Permission is hereby granted, free of charge, to any person obtaining a copy
+ *of this software and associated documentation files (the "Software"), to deal
+ *in the Software without restriction, including without limitation the rights
+ *to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *copies of the Software, and to permit persons to whom the Software is
+ *furnished to do so, subject to the following conditions:
+ *The above copyright notice and this permission notice shall be included in all
+ *copies or substantial portions of the Software.
+ *THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *SOFTWARE.
+ *
+ */
+
 #include <chrono>
 #include <condition_variable>
 #include <cstdint>
@@ -38,11 +66,12 @@ namespace utils
 	public:
 		~shared_mutex()
 		{
-			const std::lock_guard<std::mutex> lock(m_mutex);
+#ifndef NDEBUG
 			if (m_writers != 0U || m_readers != 0U || m_writers_waiting != 0U || m_readers_waiting != 0U)
 			{
 				std::terminate();
 			}
+#endif
 		}
 
 		shared_mutex() noexcept						 = default;
@@ -183,6 +212,6 @@ namespace utils
 
 		auto native_handle() -> native_handle_type { return m_mutex.native_handle(); }
 	};
-}
+}	 // namespace utils
 
 #endif	  // SHARED_MUTEX_HPP
