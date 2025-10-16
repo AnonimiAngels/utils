bc4679269e714ee4a87c595a3600ebdb4d30c2d7
Gutsulyak Vladyslav <gutsulyak.vladyslav2000@gmail.com>
2025-10-16 10:31:28 +0200

    Refactor format.hpp to improve export pragmas and remove unnecessary whitespace

--- inc/std/format.hpp
+++ inc/std/format.hpp
@@ -1,11 +1,7 @@
 #pragma once
 #ifndef FORMAT_HPP
 	#define FORMAT_HPP
-
-// IWYU pragma: begin_exports
-
 	#include "utils_macros.hpp"
-
 	#if defined(__has_include)
 		#if !MACRO_CXX20_ENABLED
 			#if __has_include(<fmt/format.h>)
@@ -31,11 +27,9 @@
 			#endif
 		#endif
 	#endif
-
 	#ifndef HAS_FMT_FORMAT
 		#define HAS_FMT_FORMAT 0
 	#endif
-
 	#if MACRO_CXX20_ENABLED
 		#if defined(__has_include)
 			#if __has_include(<format>)
@@ -44,11 +38,9 @@
 			#endif
 		#endif
 	#endif
-
 	#ifndef HAS_STD_FORMAT
 		#define HAS_STD_FORMAT 0
 	#endif
-
 	#if MACRO_CXX23_ENABLED
 		#if defined(__has_include)
 			#if __has_include(<print>)
@@ -57,34 +49,39 @@
 			#endif
 		#endif
 	#endif
-
 	#ifndef HAS_STD_PRINT
 		#define HAS_STD_PRINT 0
 	#endif
 
+// IWYU pragma: begin_exports
 	#if HAS_FMT_FORMAT
 namespace std
 {
 		#if !HAS_STD_FORMAT
+	// IWYU pragma: export
 	using fmt::format;
+	// IWYU pragma: export
 	using fmt::format_to;
+	// IWYU pragma: export
 	using fmt::format_to_n;
+	// IWYU pragma: export
 	using fmt::formatted_size;
+	// IWYU pragma: export
 	using fmt::make_format_args;
+	// IWYU pragma: export
 	using fmt::vformat;
 		#endif
-
 		#if !HAS_STD_PRINT
+	// IWYU pragma: export
 	using fmt::print;
+	// IWYU pragma: export
 	using fmt::println;
 		#endif
-
+	// IWYU pragma: export
 	using fmt::join;
 }	 // namespace std
 	#endif
-
-	#define HAS_PRINT_SUPPORT (HAS_STD_PRINT || HAS_FMT_FORMAT)
-
 // IWYU pragma: end_exports
 
+	#define HAS_PRINT_SUPPORT (HAS_STD_PRINT || HAS_FMT_FORMAT)
 #endif	  // FORMAT_HPP
